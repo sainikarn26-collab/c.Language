5d814e3e7b3d396097a8a0d728d04a99d9732b2e
karan arya <sainikarn26@gmail.com>
2025-11-07 20:15:20 +0530

    Add files via upload

---
+++ arraySum.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int findSum(int arr[], int n) {
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int main() {
+    int n;
+    printf("Enter the size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int total = findSum(arr, n);
+    printf("Sum of array elements = %d\n", total);
+
+    return 0;
+}
+
+
